f70b4fe54b02cfdb96c2a23c3736915e7e2edbcb
frecafloros <frecafloros@gmail.com>
2018-03-22 16:19:42 +0900

    Update lat2ith.js

--- docs/js/lat2ith.js
+++ docs/js/lat2ith.js
@@ -1,10 +1,18 @@
-const parser = (t) => {
+const parser = (t) =>{
+  for(let i=0; i<t.length; i++){
+    //t.charAt(i);
+    //が母音か子音かで
+  }
+}
+
+const characterize = () => {
 }
 
 $(function(){
   $('#inlat').keyup(function(){
     const t = $(this).val();
     parser(t);
+    characterize();
     $('#outith').text(t);
   });
 });
