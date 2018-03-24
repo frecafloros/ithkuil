d87559c7f899de68856c672b71264c2767c146c3
frecafloros <frecafloros@gmail.com>
2018-03-25 02:12:37 +0900

    Update lat2ith.js
    segmentation by RL

--- docs/js/lat2ith.js
+++ docs/js/lat2ith.js
@@ -1,45 +1,20 @@
-const judgelettertype = (c) =>{
-  if(c.match(/[aâeêëiîoôöuûü]/i)){
-    return "vowel";
-  }else if(c.match(/[bcčçdfghʰjklļmnňpqrřsštţvwxyzžż]/i)){
-    return "consonant";
-  }else if(c.match(/[\N{apostrophe}]/){
-    return "apostrophe";
-  }else{
-    return "unknown";
-  }
-}
-
 const segmentation = (t) =>{
-  let morpheme = [];
-  let typeflag;
-  let stack = "";
-  for(let i=0; i<t.length; i++){
-    const c = t.charAt(i);
-    const lettertype = judgelettertype(c);
-    if(i == 0){
-      typeflag = lettertype;
-    }
-    if(typeflag === lettertype || lettertype === "apostrophe"){
-      stack += c;
-    }else if(typeflag !== lettertype){
-      morpheme.push(stack);
-      typeflag = lettertype;
-      stack = c;
-    }
-  }
-  morpheme.push(stack);
-  
-  return morpheme;
+  const reg = /[aâeêëiîoôöuûü]{1,}|[bcčçdfghʰjklļmnňpqrřsštţvwxyzžż]{1,}/gi;
+  return t.match(reg);
 }
 
-const characterize = () => {
+const meaning = () =>{
+}
+ 
+const characterize = () =>{
 }
 
 $(function(){
   $('#inlat').keyup(function(){
     const t = $(this).val();
     const morpheme = segment(t);
+    console.log(morpheme);
+    meaning();
     characterize();
     $('#outith').text(t);
   });
